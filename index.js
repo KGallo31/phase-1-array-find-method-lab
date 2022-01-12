4b30e1b3193a48eebc9e27f8463e30e9430e7621
Kevin Gallo <Gallok731@gmail.com>
2022-01-12 13:00:41 -0500

    commit tree 3b4d9e81a18d4cc5b8497242a7250f0a5ff67978

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function superbowlWin(record)
+{
+    const win = record.find(i => i.result === 'W');
+    console.log(win);
+    if(win === undefined)
+    {
+        return undefined;
+    }
+    return win.year;
+}
+// const record = [
+//     { year: "2015", result: "L"},
+//     { year: "2014", result: "N/A"},
+//     { year: "2013", result: "L"},
+//     //...
+//   ]
+// superbowlWin(record);
